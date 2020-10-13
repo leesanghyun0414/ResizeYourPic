bbe772fb90c809f8e7526d5c7307b04a804d6acc
TSUYURI <tsuyuri@grit-group.co.jp>
2020-10-13 18:58:44 +0900

    モジュール化

---
+++ character_number.py
@@ -0,0 +1,11 @@
+import os
+from load_json_data import JsonData
+
+def get_character_num(character_name):
+    file_name = "chara.json"
+    json_data = JsonData(file_character_name)
+    json_data_dict = json_data.get_item()
+    if character_name in json_data_dict.keys():
+        return json_data_dict[character_name]['num']
+    else:
+        return None
