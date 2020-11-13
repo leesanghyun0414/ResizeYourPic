9b47261bc24792637b7d5e996d5e047b2a2333c0
TSUYURI <tsuyuri@grit-group.co.jp>
2020-11-13 15:30:15 +0900

    character_number 変更

--- character_number.py
+++ character_number.py
@@ -1,11 +1,11 @@
 import os
 from load_json_data import JsonData
 
-def get_character_num(character_name):
+def get_character_num(character_name, id_type):
     file_name = "chara.json"
     json_data = JsonData(file_name)
     json_data_dict = json_data.get_item()
     if character_name in json_data_dict.keys():
-        return json_data_dict[character_name]['num']
+        return json_data_dict[character_name][id_type]
     else:
         return None
