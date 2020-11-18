13a64d690a8a8d8d28b2772da18c87ce0e1d872a
TSUYURI <tsuyuri@grit-group.co.jp>
2020-11-18 15:30:41 +0900

    json関連修正

--- character_number.py
+++ character_number.py
@@ -1,10 +1,10 @@
 import os
 from load_json_data import JsonData
-
-def get_character_num(character_name, id_type):
+from collections.abc import Sequence
+def get_character_num(character_name, id_type= "num") -> Sequence:
     file_name = "chara.json"
     json_data = JsonData(file_name)
-    json_data_dict = json_data.get_item()
+    json_data_dict = json_data.get_items()
     if character_name in json_data_dict.keys():
         return json_data_dict[character_name][id_type]
     else:
